4d725549c9ef0951e6ab196823a3e993134b4984
Stephan <157989657+steynas@users.noreply.github.com>
2024-06-03 16:32:53 +0200

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 05:45:33 2024
+
+@author: SteynAS
+"""
+
+a = 1904
+b = 5
+
+print( 'Addition:\t\t' , a , '+' , b , '=' , a+b )
+print( 'Subtraction:\t' , a , '-' , b , '=' , a-b )
+print( 'Multiplication:\t' , a , '*' , b , '=' , a*b )
+print( 'Division:\t\t' , a , '/' , b , '=' , a/b )
+print( 'Floor Division:\t' , a , '//' , b , '=' , a//b )
+print( 'Modulo:\t\t\t' , a , '%' , b , '=' , a%b )
+#print('Exponent:\t\t', a, '**', b, '=', a**b, sep = ' ')
